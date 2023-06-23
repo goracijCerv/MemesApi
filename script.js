f895fa9d9314819c03fc47ed840ed47b93463af1
goracijCerv <goracijpcervantilia@gmail.com>
2023-06-23 14:45:03 -0600

    Se realizaron los ultimos cambios y ahora ya se termino

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function loadMeme(){
     var titulo = document.getElementById('head')
     var img =document.getElementById('meme')
-    
+    var txt = document.getElementById('texto')
     fetch('https://meme-api.com/gimme/raimimemes',{
         method:"GET"
     }).then((res)=>res.json())
     .then((res)=>{
-        console.log(res)
+        img.src = res.url
+        titulo.innerText = res.author
+        txt.innerText = res.title
     })
     .catch((error)=>{
         console.log(error)})
